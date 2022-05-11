a2e250c3ca432b2e7f160ed8f09fdb556afed748
Цветков Киллриал <k-real1337@mail.ru>
2022-05-11 12:09:23 +0300

    Added Ring, + Test

---
+++ JewStoreFS/Ring.fs
@@ -0,0 +1,11 @@
+﻿module Ring
+
+
+open GemData
+open Material
+open JewelryItemBase
+
+type Ring(size:int16, name: string, weight: double, material: Material, materialSample: int16, gemData: GemData, price: double, article: int64, pictureURL: string) =
+    inherit JewelryItemBase(name, weight, material, materialSample, gemData, price, article, pictureURL)
+    member this.Size = size
+    override this.ToString() = "RING" + name.ToString() + size.ToString() + weight.ToString(); 
