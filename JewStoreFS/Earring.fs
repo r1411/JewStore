10872df1198daaabb249f3e86e0ba0a746bb399d
Цветков Киллриал <k-real1337@mail.ru>
2022-05-12 10:50:03 +0300

    Подготовлены классы. Забито 8 объектов в JSON(по 2 объекта каждого типа

--- JewStoreFS/Earring.fs
+++ JewStoreFS/Earring.fs
@@ -5,7 +5,7 @@ open GemData
 open Material
 open JewelryItemBase
 
-type Earring(length:int16, haslock:bool, name: string, weight: double, material: Material, materialSample: int16, gemData: GemData, price: double, article: int64, pictureURL: string) =
+type Earring(length:int16, haslock:bool, name: string, weight: double, material: Material, materialSample: int16, gemData: Option<GemData>, price: double, article: int64, pictureURL: string) =
     inherit JewelryItemBase(name, weight, material, materialSample, gemData, price, article, pictureURL)
     member this.Length = length
     member this.HasLock = haslock
