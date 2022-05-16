b8d57eb8d0ee8a6139f3c9ef00f505365be40b7a
r1411 <roma00055@gmail.com>
2022-05-16 14:49:08 +0300

    Рабочая загрузка из файла в левый список

--- JewStoreFront/FrontForm.cs
+++ JewStoreFront/FrontForm.cs
@@ -16,15 +16,53 @@ namespace JewStoreFront
             this.db = new JsonDB(dbPath);
             
             InitializeComponent();
-            listBox.Items.Clear();
             comboBoxType.SelectedIndex = 0;
-            comboBoxSorting.SelectedIndex = 0;
+            comboBoxSorting.SelectedIndex = 3;
         }
 
         // Обновить список товаров в левой части экрана
         private void UpdateItemsList()
         {
-            
+            FSharpList<JewelryItemBase> list = FSharpList<JewelryItemBase>.Empty;
+            switch (comboBoxType.SelectedIndex)
+            {
+                case 0:
+                    list = db.GetAllJewelry();
+                    break;
+                case 1:
+                    list = db.GetRings();
+                    break;
+                case 2:
+                    list = db.GetEarrings();
+                    break;
+                case 3:
+                    list = db.GetChains();
+                    break;
+                case 4:
+                    list = db.GetWatches();
+                    break;
+            }
+
+            Func<JewelryItemBase, JewelryItemBase, int> sortingFunc = (a, b) => 0;
+            switch (comboBoxSorting.SelectedIndex)
+            {
+                case 0:
+                    sortingFunc = (a, b) => a.Name.CompareTo(b.Name);
+                    break;
+                case 1:
+                    sortingFunc = (a, b) => b.Name.CompareTo(a.Name);
+                    break;
+                case 2:
+                    sortingFunc = (a, b) => a.Price.CompareTo(b.Price);
+                    break;
+                case 3:
+                    sortingFunc = (a, b) => b.Price.CompareTo(a.Price);
+                    break;
+            }
+            this.selectedItems = JsonDB.Sorted(list, FuncConvert.FromFunc(sortingFunc));
+
+            listBox.Items.Clear();
+            ListModule.Iterate(FuncConvert.FromAction<JewelryItemBase>(x => listBox.Items.Add(x.Name)), selectedItems);
         }
 
         private void comboBoxType_SelectedIndexChanged(object sender, System.EventArgs e)
